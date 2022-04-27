b6a2e6bef5a644c93a018cd74715e7ff5fd1443b
SamarthJain21 <jainsamarth786@gmail.com>
2022-04-27 16:03:11 +0530

    trader item api

--- server/routes/traderRoutes.js
+++ server/routes/traderRoutes.js
@@ -31,4 +31,74 @@ router.post('/addTrader',(req,res)=>{
     
 });
 
+router.post('/deleteTrader',(req,res)=>{
+    console.log("Remove Trader request"+ req.body.companyGST);
+
+    db.query(
+        "delete from `traders`where `companyGST`=? and`gstNo`=?",
+        [req.body.companyGST,req.body.gstNo],
+        (err,result)=>{
+            if(err){
+                res.send(err);
+            }else{
+                res.send("Trader Removed if existed");
+            }
+        }
+    );
+
+    
+});
+
+router.get('/getTraders',(req,res)=>{
+    console.log("Get Trader request"+ req.body.companyGST);
+
+    db.query(
+        "select * from `traders`where `companyGST`=?",
+        [req.body.companyGST],
+        (err,result)=>{
+            if(err){
+                res.send(err);
+            }else{
+                res.send(result);
+            }
+        }
+    );
+
+    
+});
+
+router.post('/addItem',(req,res)=>{
+    console.log("Request to add item for a trader ",req.body.companyGST);
+    
+    db.query(
+        "Insert into `traderItems`(`traderID`,`itemID`) values(?,?)",
+        [req.body.traderID,req.body.itemID],
+        (err,result)=>{
+            if(err){
+                res.send(err);
+            }else{
+                res.send("Trader Item Added");
+            }
+        }
+    );
+
+})
+
+router.post('/deleteItem',(req,res)=>{
+    console.log("Request to delete item for a trader ",req.body.companyGST);
+    
+    db.query(
+        "delete from `traderItems` where id=?",
+        [req.body.itemID],
+        (err,result)=>{
+            if(err){
+                res.send(err);
+            }else{
+                res.send("Item if existed was deleted");
+            }
+        }
+    );
+
+});
+
 export default router;
